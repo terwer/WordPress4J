8073c85872e1024d50acb311775e2f3d6bacab1a
terwer <youweics@163.com>
2022-09-16 00:24:51 +0800

    feat:#1 项目初始化

---
+++ src/main/java/space/terwer/wordpress4j/Wordpress4jApplication.java
@@ -0,0 +1,20 @@
+package space.terwer.wordpress4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * wordpress4j入口
+ *
+ * @name: Wordpress4jApplication
+ * @author: terwer
+ * @date: 2022-09-16 00:18
+ **/
+@SpringBootApplication
+public class Wordpress4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wordpress4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/space/terwer/wordpress4j/WordPress4JApplicationTests.java
@@ -0,0 +1,20 @@
+package space.terwer.wordpress4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 单元测试入口
+ *
+ * @name: WordPress4JApplicationTests
+ * @author: terwer
+ * @date: 2022-09-16 00:24
+ **/
+@SpringBootTest
+class WordPress4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
